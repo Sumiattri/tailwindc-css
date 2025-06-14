5a0da3dc972f2ac3bc5006544158dc77a8bd6a0a
Sumit Attri <sumitattri165@gmail.com>
2025-06-15 01:09:23 +0530

    modified the dropdown

--- 03-course-website/src/components/DropDownMenu.jsx
+++ 03-course-website/src/components/DropDownMenu.jsx
@@ -7,34 +7,34 @@ function DropDownMenu({ setIsOpen }) {
         className="fixed h-screen w-screen bg-gray-500 opacity-25 z-1"
         onClick={() => setIsOpen(false)}
       ></div>
-      <div className="absolute top-[72px] left-0 w-full bg-white px-6 py-4 shadow-md z-40 md:hidden flex flex-col gap-4 justify-center">
+      <div className="absolute top-[72px] left-0 w-screen bg-white  py-4 shadow-md z-40 md:hidden flex flex-col gap-4 justify-center">
         <a
           href="#"
           onClick={() => setIsOpen(false)}
-          className="pl-4 shadow-xs shadow-gray-300  text-lg"
+          className="pl-4 shadow-xs shadow-gray-300  text-lg "
         >
-          Home
+          <span className="ml-5">Home</span>
         </a>
         <a
           href="#"
           onClick={() => setIsOpen(false)}
           className="pl-4 shadow-xs shadow-gray-300  text-lg"
         >
-          Store
+          <span className="ml-5">Store</span>
         </a>
         <a
           href="#"
           onClick={() => setIsOpen(false)}
           className="pl-4  shadow-xs shadow-gray-300 text-lg"
         >
-          Career
+          <span className="ml-5">Career</span>
         </a>
         <a
           href="#"
           onClick={() => setIsOpen(false)}
           className="pl-4 text-lg shadow-xs shadow-gray-300 "
         >
-          About
+          <span className="ml-5">About</span>
         </a>
       </div>
     </>
